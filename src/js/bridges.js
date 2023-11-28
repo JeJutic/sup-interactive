37494f601409e0d2b5b4418ab4e61c67ff0fec9d
Blayzzand <368833@edu.itmo.ru>
2023-11-28 20:16:19 +0300

    Edit the pop-up question card

--- src/js/bridges.js
+++ src/js/bridges.js
@@ -1,17 +1,28 @@
 import React from "react";
 import DefaultQuestion from "../questions/DefaultQuestion";
+import testBridge from "../img/test-bridge.jpg";
 
 export const bridges = [
   {
-    "position": [59.92518, 30.32687],
-    "overlayComponent": (
-        <DefaultQuestion question={"Вопрос?"}/>
-    )
+    position: [59.92518, 30.32687],
+    overlayComponent: (
+      <DefaultQuestion
+        question={"Вопрос?"}
+        image={testBridge}
+        header="Card Name"
+        description="lorem ipsum чето то там и так идет описание карточки наверное это можно было сделать через children элемента чтобы было удобнее но мне пока лень это делать"
+      />
+    ),
   },
   {
-    "position": [59.94215, 30.33812],
-    "overlayComponent": (
-        <DefaultQuestion question={"Чижик-пыжик?"}/>
-    )
-  }
-]
+    position: [59.94215, 30.33812],
+    overlayComponent: (
+      <DefaultQuestion
+        question={"Чижик-пыжик?"}
+        image="https://placekitten.com/500/500"
+        header="Card Name"
+        description="lorem ipsum"
+      />
+    ),
+  },
+];
