6cb4438e84c6134da7a38e21a23247ab54a6785b
JeJutic <kinga8171@gmail.com>
2023-12-05 14:09:45 +0500

    added prettier for code formatting

--- src/commons/WelcomeScreen.jsx
+++ src/commons/WelcomeScreen.jsx
@@ -2,21 +2,21 @@ import { useState } from "react";
 import "./WelcomeScreen.css";
 
 export const WelcomeScreen = () => {
-    const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(true);
 
-    const handleClick = () => setIsVisible(false);
+  const handleClick = () => setIsVisible(false);
 
-    return (
-        <>
-            <div className={`welcome-screen ${isVisible ? "" : "hidden"}`}>
-                <h1 className="welcome-screen-name">Приветственный Экран!</h1>
-                <p className="welcome-screen-description">
-                    Здесь могла быть ваша реклама (ну или описание проекта)
-                </p>
-                <button className="welcome-screen__btn" onClick={handleClick}>
-                    Понял
-                </button>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={`welcome-screen ${isVisible ? "" : "hidden"}`}>
+        <h1 className="welcome-screen-name">Приветственный Экран!</h1>
+        <p className="welcome-screen-description">
+          Здесь могла быть ваша реклама (ну или описание проекта)
+        </p>
+        <button className="welcome-screen__btn" onClick={handleClick}>
+          Понял
+        </button>
+      </div>
+    </>
+  );
 };
--- src/js/App.js
+++ src/js/App.js
@@ -1,40 +1,43 @@
-import '../css/App.css';
-import {MapContainer, TileLayer, useMap} from "react-leaflet";
+import "../css/App.css";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import L from "leaflet";
 import React from "react";
 import BridgeMarkerList from "../commons/BridgeMarker";
-import {WelcomeScreen} from "../commons/WelcomeScreen";
+import { WelcomeScreen } from "../commons/WelcomeScreen";
 
-const center = [59.928, 30.314]
-const defaultZoom = 14
-const minZoom = 13
+const center = [59.928, 30.314];
+const defaultZoom = 14;
+const minZoom = 13;
 
 function App() {
-    return (
-        <>
-            <WelcomeScreen />
-            <MapContainer className="map" style={{ height: "100vh", width: "100%" }}
-                          center={center}
-                          zoom={defaultZoom} scrollWheelZoom={false}
-                          attributionControl={false}
-            >
-                <MapSettings/>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <BridgeMarkerList/>
-            </MapContainer>
-        </>
-    );
+  return (
+    <>
+      <WelcomeScreen />
+      <MapContainer
+        className="map"
+        style={{ height: "100vh", width: "100%" }}
+        center={center}
+        zoom={defaultZoom}
+        scrollWheelZoom={false}
+        attributionControl={false}
+      >
+        <MapSettings />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <BridgeMarkerList />
+      </MapContainer>
+    </>
+  );
 }
 
 function MapSettings() {
-    const map = useMap()
-    map.setMinZoom(minZoom)
-    const bounds = L.latLng(center[0], center[1]).toBounds(12000)
-    map.setMaxBounds(bounds)
-    return null
+  const map = useMap();
+  map.setMinZoom(minZoom);
+  const bounds = L.latLng(center[0], center[1]).toBounds(12000);
+  map.setMaxBounds(bounds);
+  return null;
 }
 
 export default App;
