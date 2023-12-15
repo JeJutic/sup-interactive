be9af93f5262947e916eb313a1e6698ad5426457
Timur Aminev <ablearthy@gmail.com>
2023-12-16 01:30:15 +0300

    kek

--- src/js/App.js
+++ src/js/App.js
@@ -121,6 +121,19 @@ const smallLabels = [
     [59.925001, 30.237894, "Севкабель порт"]
 ];
 
+const bigLabels = [
+    [59.957610, 30.288491, "Петроградский район"],
+    [59.938117, 30.250082, "Василеостровский район"],
+    [59.907404, 30.242271, "Кировский район"],
+    [59.906156, 30.325154, "Московский район"],
+    [59.911062, 30.350761, "Фрунзенский район"],
+    [59.914122, 30.423632, "Невский район"],
+    [59.941686, 30.427151, "Красногвардейский район"],
+    [59.941299, 30.367211, "Центральный район"],
+    [59.961026, 30.371704, "Калининский район"],
+    [59.960940, 30.343865, "Выборгский район"]
+];
+
 const defaultZoom = 14;
 const minZoom = 14;
 
@@ -209,7 +222,7 @@ function App() {
     map.setMaxBounds(bounds);
     var zsh = new ZoomShowHide();
     zsh.addTo(map);
-      for (const [lat, lon, text, rotate, maxWidth] of smallLabels) {
+    for (const [lat, lon, text, rotate, maxWidth] of smallLabels) {
         let maxWidthStyle = "";
         if (maxWidth !== undefined) {
             maxWidthStyle = `max-width: ${maxWidth}px`
@@ -226,6 +239,17 @@ function App() {
             })
         })
         zsh.addLayer(marker, 14.5, null);
+    }
+    for (const [lat, lon, text] of bigLabels) {
+        const innerContent = text.split(' ').map(w => `<div>${w}</div>`).join('');
+        let content = `<div class="leaflet-big-label-inner">${innerContent}</div>`;
+        L.marker([lat, lon], {
+            icon: L.divIcon({
+                html: content,
+                className: 'leaflet-big-label',
+                iconSize: null,
+            })
+        }).addTo(map);
     }
 
     var popup = L.popup()
