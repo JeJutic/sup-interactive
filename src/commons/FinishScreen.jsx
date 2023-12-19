88316302c1bd50286213f74affc33adbcddf2dbf
Michael <pieceoftartofficial@gmail.com>
2023-12-19 03:06:44 +0300

    add: finish page

--- src/commons/FinishScreen.jsx
+++ src/commons/FinishScreen.jsx
@@ -1,19 +1,24 @@
-import "./WelcomeScreen.css";
+// FinishScreen.jsx
+import React from "react";
+import "./FinishScreen.css";
 
-export const FinishScreen = () => {
+const FinishScreen = () => {
   const handleClick = () => window.location.reload();
 
   return (
-    <div>
-      <div className={`welcome-screen`}>
-        <h1 className="welcome-screen-name">Экран завершения</h1>
-        <p className="welcome-screen-description">
-          Поздравляем с прохождением. Вы можете перепройти, перезагрузив
-          страницу.
+    <div className="frame">
+      <div className="content">
+        <h1 className="congrats-header">ПРИВЕТСТВЕННЫЙ ЭКРАН</h1>
+        <p className="p">
+          Этот праздник уже восемь лет вместе с нами создаете вы — своей энергией, прекрасными костюмами и настроением!
+          Всем легкой воды, всем счастья. Петербург — лучший. Это «Фонтанка.ру», это — «Фонтанка SUP
         </p>
-        <button className="welcome-screen__btn" onClick={handleClick}>
-          Перезагрузить
-        </button>
+        <div className="button-wrapper">
+          <button className="button-inside" onClick={handleClick}>Поплыли</button>
+        </div>
+      </div>
+      <div className="bottom__img__wrapper">
+        <div className="bottom__img"></div>
       </div>
     </div>
   );
