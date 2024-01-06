2e858495ae668c56466d51e391d7331d3da87986
Timur Aminev <ablearthy@gmail.com>
2024-01-06 15:44:56 +0300

    Add ability to customize content grid layout

--- src/lib/card/ContentGridLayout.jsx
+++ src/lib/card/ContentGridLayout.jsx
@@ -1,10 +1,10 @@
 import styles from "./ContentGridLayout.module.css";
 
-function ContentGridLayout({ title, content, sidebar }) {
+function ContentGridLayout({ title, content, sidebar, customStyles }) {
   return (
     <div className={styles.grid}>
       <div className={styles.title}>{title}</div>
-      <div className={styles.sidebar}>{sidebar}</div>
+      <div className={styles.sidebar} style={customStyles && customStyles.sidebar}>{sidebar}</div>
       <div className={styles.content}>{content}</div>
     </div>
   );
