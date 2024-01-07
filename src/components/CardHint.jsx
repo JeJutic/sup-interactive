9f5f259a8a663b8e8316fbca8ec237d130a0cb31
Timur Aminev <ablearthy@gmail.com>
2024-01-07 18:17:24 +0300

    Implement CardHint

--- src/components/CardHint.jsx
+++ src/components/CardHint.jsx
@@ -1,8 +1,39 @@
 import Button from "lib/Button";
 import styles from "./CardHint.module.css";
+import { useState, Fragment } from "react";
+import Modal from "react-overlays/Modal";
 
 function CardHint({ hint }) {
-  return <Button className={styles["hint-button"]}>?</Button>;
+  const [showHint, setShowHint] = useState(false);
+  console.log(Modal);
+  return (
+    <>
+      <Button
+        onClick={() => setShowHint(true)}
+        className={styles["hint-button"]}
+      >
+        ?
+      </Button>
+      <Modal
+        className={styles["modal"]}
+        show={showHint}
+        onHide={() => setShowHint(false)}
+      >
+        <Fragment>
+          <div className={styles["modal-content"]}>
+            <h3>Подсказка</h3>
+            <div className={styles["modal-content-hint"]}>{hint}</div>
+          </div>
+          <Button
+            onClick={() => setShowHint(false)}
+            className={styles["modal-close"]}
+          >
+            &#215;
+          </Button>
+        </Fragment>
+      </Modal>
+    </>
+  );
 }
 
 export default CardHint;
